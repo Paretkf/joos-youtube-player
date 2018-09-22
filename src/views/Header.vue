3d96c3a995b91d2f46ea5a1b4aa2a32d43931c5b
Paretkf <paretk7@gmail.com>
2018-09-22 23:53:11 +0700

    edit fb loggin

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,5 +1,9 @@
 <template>
-   <div class="home w-100pct bg-cl-info dp-flex jtf-ct-flex-end al-it-center pd-hrzt-20px">
+<div class="home w-100pct bg-cl-info dp-flex jtf-ct-space-between">
+  <div class="home w-50pct dp-flex jtf-ct-flex-start al-it-center pd-hrzt-20px">
+    <span class="f-s-25px f-w-bold cl-white cs-pointer">+Album</span>
+  </div>
+  <div class="home w-50pct dp-flex jtf-ct-flex-end al-it-center pd-hrzt-20px">
     <span @click="login()" class="f-s-25px f-w-bold cl-white cs-pointer" v-if="!user.user"> Login With Facebook</span>
     <div v-else class="dp-flex al-it-center">
       <img :src="user.user.photoURL" alt="" class="bd-rd-20px mg-r-10px">
@@ -9,6 +13,7 @@
       <div @click="logout()" class="f-s-25px f-w-bold cl-white cs-pointer"> Logout </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
