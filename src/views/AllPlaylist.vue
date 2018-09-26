507a5d0cd8d1a874bce5221ed0183889770dfa95
Paretkf <paretk7@gmail.com>
2018-09-27 01:37:59 +0700

    debug/display item

--- src/views/AllPlaylist.vue
+++ src/views/AllPlaylist.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="wrapper">
-    <div class="pin" id="columns">
-      <div v-for="(album, index) in allAlbums" :key="index" :title="album.name" @click="setAlbum(album)" class="pin cs-pointer card pd-5px w-fit-content t-al-center  mg-bt-10px">
+    <div class="dp-flex f-drt-row f-w-wrap t-al-center ctn">
+      <div class="cs-pointer card pd-5px w-fit-content h-fit-content mg-hrzt-10px t-al-center mg-bt-10px" v-for="(album, index) in allAlbums" :key="index" :title="album.name" @click="setAlbum(album)">
         <!-- {{allAlbums}} -->
         <img :src="album.img" :alt="album.name" width="150"> <br>
         <span class="f-w-bold sweet-label mg-auto">
@@ -42,40 +42,8 @@ export default {
 </script>
 
 <style scoped>
-.pin {
-   display: inline-block;
-  -webkit-column-break-inside: avoid;
-  -moz-column-break-inside: avoid;
-  -webkit-transition: all .2s ease;
-  -moz-transition: all .2s ease;
-  -o-transition: all .2s ease;
-  transition: all .2s ease;
+.ctn {
+  align-items: center;
+  justify-content: center;
 }
-.item {
-    /* position: sticky; */
-    display: inline-block;
-    /* float: left; */
-  }
-@media (min-width: 960px) {
-  #columns {
-    -webkit-column-count: 4;
-    -moz-column-count: 4;
-    column-count: 4;
-    overflow: auto;
-  }
-}
-@media (min-width: 1100px) {
-  #columns {
-    -webkit-column-count: 5;
-    -moz-column-count: 5;
-    column-count: 5;
-    overflow: auto;
-  }
-}
-#wrapper {
-    width: 90%;
-    max-width: 1100px;
-    min-width: 800px;
-    margin: 0px auto;
-  }
 </style>
